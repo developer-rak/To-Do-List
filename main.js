7e4cb7e93a427d2f948282636bb55694ad2a9c11
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-04-07 23:06:28 +0500

    img

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ window.addEventListener('load', () => {
 
         const task = input.value;
 
-        // if (!task) {
-        //     alert("Please fill out the task");
-        //     return;
-        // }
+        if (!task) {
+            alert("Please fill out the task");
+            return;
+        }
 
         const task_el = document.createElement("div");
         task_el.classList.add("task");
